c3596b2dafc44c198e9903a84f13b2fe1215e769
Paul WOISARD <paulwoisard@gamil.com>
2020-12-18 00:34:07 +0100

    info message

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ client.on('message', message => {
   
   const list = client.guilds.cache.get("472687107530555402");
   if (message.author.id !== "777238000316055553" && (message.content.includes('https://') || message.content.includes('http://') || message.content.includes('www.') || message.content.includes('.fr') || message.content.includes('.be') || message.content.includes('.com') || message.content.includes('.uk') || message.content.includes('.de') || message.content.includes('.org'))) {
-    list.members.cache.every(member => {
-     if (member.roles.cache.some(role => role.name === 'Administrateur' || 'Suppléant-Admin' || '**Moderateur**')) {
+    list.members.cache.forEach(member => {
+     if (member.roles.cache.some(role => role.name === 'Administrateur') || member.roles.cache.some(role => role.name ==='Suppléant-Admin') || member.roles.cache.some(role => role.name ==='**Moderateur**')) {
         member.send(message.author.username + ' a envoyé ce message ***'  + message.content + '*** sur le salon ' + message.channel.name);
-        return false;
      }
-     return true;
    });
   }
 });
