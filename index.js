eb0834ced468b353cf2f84543f0e35c1541058c7
Paul WOISARD <paulwoisard@gamil.com>
2020-12-20 11:18:10 +0100

    ajout code @Desmos, envoi message au staff si un nouvel utilisateur s'est connecté depuis 24h

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+list_new_user = [];
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-  if (
-    message.content.toLowerCase().startsWith("hello") ||
-    message.content.toLowerCase().startsWith("bonjour") ||
-    message.content.toLowerCase().startsWith("bonsoir") ||
-    message.content.toLowerCase().startsWith("coucou") ||
-    message.content.toLowerCase().startsWith("hey") ||
-    message.content.toLowerCase().startsWith("salut")
-  ) {
-    message.react('👋');
-  }
-
-  const list = client.guilds.cache.get("472687107530555402");
-
-  if (message.author.id !== "777238000316055553" && (message.content.includes('https://') || message.content.includes('http://') || message.content.includes('www.') || message.content.includes('.fr') || message.content.includes('.be') || message.content.includes('.com') || message.content.includes('.uk') || message.content.includes('.de') || message.content.includes('.org'))) {
+function EnvoiMessageAdmin(messageToAdmin){
+    const list = client.guilds.cache.get("472687107530555402");
     list.members.cache.forEach(member => {
-     if (member.roles.cache.some(role => role.name === 'Administrateur')) {
-        member.send(message.author.username + ' a envoyé ce message ***'  + message.content + '*** sur le salon ' + message.channel.name);
-     } else if (member.roles.cache.some(role => role.name ==='Suppléant-Admin')) {
-        member.send(message.author.username + ' a envoyé ce message ***'  + message.content + '*** sur le salon ' + message.channel.name);
-     } else if (member.roles.cache.some(role => role.name ==='**Moderateur**')) {
-        member.send(message.author.username + ' a envoyé ce message ***'  + message.content + '*** sur le salon ' + message.channel.name);
-     }
-   });
-  }
+        if (member.roles.cache.some(role => role.name === 'Administrateur')) {
+           member.send(messageToAdmin);
+        } else if (member.roles.cache.some(role => role.name ==='Suppléant-Admin')) {
+           member.send(messageToAdmin);
+        } else if (member.roles.cache.some(role => role.name ==='**Moderateur**')) {
+           member.send(messageToAdmin);
+        }
+      });
+}
+
+client.on('guildMemberAdd', member => {
+    heure_message = 24 * 3600 * 1000;
+    new_user = member.user.username;
+    list_new_user.push(new_user);
+    setTimeout(function (){
+        message_to_send =  list_new_user[0] + " est arrivé sur le serveur depuis 24h merci de bien vouloir vérifier s'il s'est présenté dans le channel présentation !";
+        EnvoiMessageAdmin(message_to_send);
+        list_new_user.splice(0, 1);
+    }, heure_message)
+});
+
+client.on('message', message => {
+    if (
+      message.content.toLowerCase().startsWith("hello") ||
+      message.content.toLowerCase().startsWith("bonjour") ||
+      message.content.toLowerCase().startsWith("bonsoir") ||
+      message.content.toLowerCase().startsWith("coucou") ||
+      message.content.toLowerCase().startsWith("hey") ||
+      message.content.toLowerCase().startsWith("salut")
+    ) {
+      message.react('👋');
+    }
+    const list = client.guilds.cache.get("472687107530555402");
+
+    if (message.author.id !== "777238000316055553" && (message.content.includes('https://') || message.content.includes('http://') || message.content.includes('www.') || message.content.includes('.fr') || message.content.includes('.be') || message.content.includes('.com') || message.content.includes('.uk') || message.content.includes('.de') || message.content.includes('.org'))) {
+      message_to_send = member.send(message.author.username + ' a envoyé ce message ***'  + message.content + '*** sur le salon ' + message.channel.name);
+      EnvoiMessageAdmin(message_to_send);
+    }
+
 });
 
 client.login(process.env.Token);
